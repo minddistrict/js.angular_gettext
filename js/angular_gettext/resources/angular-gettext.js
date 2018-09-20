f54bf4743d3b2757ce56867d3a6713062f44358a
Sylvain Viollon <sviollon@minddistrict.com>
2018-09-20 11:49:44 +0200

    remove a constant we want to define in a different way.

--- js/angular_gettext/resources/angular-gettext.js
+++ js/angular_gettext/resources/angular-gettext.js
@@ -25,32 +25,6 @@
  */
 angular.module('gettext', []);
 /**
- * @ngdoc object
- * @module gettext
- * @name gettext
- * @kind function
- * @param {String} str annotation key
- * @description Gettext constant function for annotating strings
- *
- * ```js
- * angular.module('myApp', ['gettext']).config(function(gettext) {
- *   /// MyApp document title
- *   gettext('my-app.title');
- *   ...
- * })
- * ```
- */
-angular.module('gettext').constant('gettext', function (str) {
-    /*
-     * Does nothing, simply returns the input string.
-     *
-     * This function serves as a marker for `grunt-angular-gettext` to know that
-     * this string should be extracted for translations.
-     */
-    return str;
-});
-
-/**
  * @ngdoc service
  * @module gettext
  * @name gettextCatalog
